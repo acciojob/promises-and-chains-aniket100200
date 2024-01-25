aa4e02072595cb4d2bf5f46a2d22c290bc2b1acd
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2024-01-25 16:37:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let form=document.querySelector("form");
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+ let age=e.target.age.value;
+
+ let name=e.target.name.value;
+  
+ canYouVote(age,name)
+ .then((data)=>{
+  alert(data);
+ })
+ .catch((err)=>{
+  alert(err);
+ })
+});
+
+function canYouVote(age,name) {
+  return new Promise((resolve,reject)=>{
+    setTimeout(() => {
+      if(age>=18){
+        resolve(`Welcome, ${name}. You can vote.`);
+      }
+      else reject(`Oh sorry ${name}. You aren't old enough.`)
+      
+    }, 4000);
+  });
+}
